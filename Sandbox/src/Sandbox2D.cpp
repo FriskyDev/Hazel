f8766a2f788dc9ab01087826dce83c9a16d4ee41
Chuck England (FriskyDev) <crystal_bit@hotmail.com>
2020-05-20 17:06:29 -0700

    Add a sample map as a string, and setup tile textures to render it.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -4,6 +4,24 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
+// W = Water
+// D = Dirt
+static const uint32_t s_MapWidth = 24;
+static const char* s_MapTiles =
+"WWWWWWWWWWWWWWWWWWWWWWWW"
+"WWWWWWWDDDDDDWWWWWWWWWWW"
+"WWWWDDDDDDDDDDDDDWWWWWWW"
+"WWWDDDDDDDDDDDCDDDDDWWWW"
+"WWDDDDWWDDDDDDDDDDDDDWWW"
+"WDDDDDWWDDDDDDDDDDDDDDWW"
+"WWDDDDDDDDDDDDDDDDDDDWWW"
+"WWWWDDDDDDDDDDDDDDDDWWWW"
+"WWWWWDDDDDDDDDDDDDWWWWWW"
+"WWWWWWDDDDDDDDDDWWWWWWWW"
+"WWWWWWWWWDDDDWWWWWWWWWWW"
+"WWWWWWWWWWWWWWWWWWWWWWWW";
+
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f), m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
 {
@@ -16,9 +34,14 @@ void Sandbox2D::OnAttach()
 	m_CheckerboardTexture = Hazel::Texture2D::Create("assets/textures/Checkerboard.png");
 	m_SpriteSheet = Hazel::Texture2D::Create("assets/game/textures/RPGpack_sheet_2X.png");
 
-	m_TextureStairs = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 7, 6 }, { 128, 128 });
-	m_TexureBarrel = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 8, 2 }, { 128, 128 });
-	m_TextureTree = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 2, 1 }, { 128, 128 }, { 1, 2 });
+	m_TextureStairs = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 5, 11 }, { 128, 128 });
+	m_TextureTree = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 2, 1 }, { 128, 128 });
+	m_TexureBarrel = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 2, 1 }, { 128, 128 }, { 1, 2 });
+
+	m_MapWidth = s_MapWidth;
+	m_MapHeight = strlen(s_MapTiles) / s_MapWidth;
+	s_TextureMap['D'] = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 6, 11 }, { 128, 128 });
+	s_TextureMap['W'] = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 11, 11 }, { 128, 128 });
 
 	m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
 	m_Particle.ColorEnd = { 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f };
@@ -27,6 +50,8 @@ void Sandbox2D::OnAttach()
 	m_Particle.Velocity = { 0.0f, 0.0f };
 	m_Particle.VelocityVariation = { 3.0f, 1.0f };
 	m_Particle.Position = { 0.0f, 0.0f };
+
+	m_CameraController.SetZoomLevel(5.0f);
 }
 
 void Sandbox2D::OnDetach()
@@ -78,6 +103,7 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 
 	// particle system
 	if (Hazel::Input::IsMouseButtonPressed(HZ_MOUSE_BUTTON_LEFT)) {
+		HZ_PROFILE_SCOPE("Emit Particles");
 		auto [x, y] = Hazel::Input::GetMousePosition();
 		auto width = Hazel::Application::Get().GetWindow().GetWidth();
 		auto height = Hazel::Application::Get().GetWindow().GetHeight();
@@ -87,20 +113,44 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 		x = (x / width) * bounds.GetWidth() - bounds.GetWidth() * 0.5f;
 		y = bounds.GetHeight() * 0.5f - (y / height) * bounds.GetHeight();
 		m_Particle.Position = { x + pos.x, y + pos.y };
+
 		constexpr auto num_particles_to_emit = 5;
-		for (int i = 0; i < num_particles_to_emit; i++)
+		for (int i = 0; i < num_particles_to_emit; i++) {
 			m_ParticleSystem.Emit(m_Particle);
+		}
+	}
+	{
+		HZ_PROFILE_SCOPE("Update Particles");
+		m_ParticleSystem.OnUpdate(ts);
+	}
+	{
+		HZ_PROFILE_SCOPE("Render Particles");
+		m_ParticleSystem.OnRender(m_CameraController.GetCamera());
 	}
 
-	m_ParticleSystem.OnUpdate(ts);
-	m_ParticleSystem.OnRender(m_CameraController.GetCamera());
+	{
+		// sprite sheet
+		HZ_PROFILE_SCOPE("Game Scene Render");
+		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (uint32_t y = 0 ; y < m_MapHeight; y++) {
+			for (uint32_t x = 0 ; x < m_MapWidth; x++) {
+				char tileType = s_MapTiles[x + y * m_MapWidth];
+				Hazel::Ref<Hazel::SubTexture2D> texture;
+				if (s_TextureMap.find(tileType) != s_TextureMap.end()) {
+					texture = s_TextureMap[tileType];
+				} else {
+					texture = m_TextureStairs;
+				}
 
-	// sprite sheet
-	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TextureStairs);
-	Hazel::Renderer2D::DrawQuad({ 1.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TexureBarrel);
-	Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f, 0.5f }, { 1.0f, 2.0f }, m_TextureTree);
-	Hazel::Renderer2D::EndScene();
+				Hazel::Renderer2D::DrawQuad({ x - m_MapWidth / 2.0f, m_MapHeight - y - m_MapHeight / 2.0f, 0.5f }, { 1.0f, 1.0f }, texture);
+			}
+		}
+
+		//Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TextureStairs);
+		//Hazel::Renderer2D::DrawQuad({ 1.0f, 0.0f, 0.5f }, { 1.0f, 1.0f }, m_TexureBarrel);
+		//Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f, 0.5f }, { 1.0f, 2.0f }, m_TextureTree);
+		Hazel::Renderer2D::EndScene();
+	}
 }
 
 void Sandbox2D::OnImGuiRender()
